7dd640c3b5544dab474405f3ab63efddb94201ef
holgus103 <kubasuchan@hotmail.com>
2017-09-23 22:31:25 +0200

    Preprocessing as module

--- main.jl
+++ main.jl
@@ -1,15 +1,3 @@
-using FileIO;
+include("preprocessing.jl");
 
-img = load("./img/2.png");
-h, w = size(img);
-data = Array{Bool}(h,w); 
-for j = 1:h
-    for i = 1:w
-        c = img[j, i];
-        if(c.r == 0 && c.g == 0 && c.b == 0)
-            data[j,i] = false;
-        else
-            data[j,i] = true;
-        end
-    end
-end
+preprocessing.preprocessRGBImage("./img/1.png");
---
+++ preprocessing.jl
@@ -0,0 +1,23 @@
+module preprocessing;
+using FileIO;
+using FixedPointNumbers;
+
+    function preprocessRGBImage(path)
+        img = load(path);
+        h, w = size(img);
+        data = Array{Bool}(h,w);
+        z = Normed{UInt8, 8}(0); 
+        for j = 1:h
+            for i = 1:w
+                c = img[j, i];
+                if(c.r == z && c.g == z && c.b == z)
+                    data[j,i] = false;
+                else
+                    data[j,i] = true;
+                end;
+            end;
+        end;
+    return data;
+    end;
+
+end;
